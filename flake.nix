c7fac4e94c774b66d92a6ce719e3bd9eb9a9d661
6z7y <eenanho24n@proton.me>
2026-01-19 11:53:53 +0300

    test nixx2

--- flake.nix
+++ flake.nix
@@ -42,17 +42,8 @@
 
 {
   description = "Tomu";
-
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
   outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-      buildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
-        gcc
-        ffmpeg
-        pkg-config
-        make
-      ];
-    };
+    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.callPackage ./default.nix {};
   };
 }
