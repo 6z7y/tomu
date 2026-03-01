fb73c52f5c784666035c0c9ba8f7bb47ce2806d4
6z7y <eenanho24n@proton.me>
2026-03-01 23:06:01 +0300

    add shared headers

---
+++ src/clients/share-clients.h
@@ -0,0 +1,78 @@
+#ifndef SHARE_CLIENTS_H
+#define SHARE_CLIENTS_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+typedef enum {
+  CMD_PLAY_TOGGLE,
+  CMD_STOP,
+  CMD_NEXT_AUDIO,
+  CMD_PREV_AUDIO,
+  CMD_VOL_UP,
+  CMD_VOL_DOWN,
+  CMD_SPEED_SLOW,
+  CMD_SPEED_FAST,
+  CMD_SPEED_DEFAULT,
+  CMD_SEEK_FORWARD_5S,
+  CMD_SEEK_FORWARD_1M,
+  CMD_SEEK_BACKWARD_5S,
+  CMD_SEEK_BACKWARD_1M,
+  CMD_LOOP_TOGGLE,
+  CMD_SHUFFLE_TOGGEL,
+  CMD_PATH
+
+} Command;
+
+typedef struct {
+  char *key;
+  Command cmd;
+} KeyMap;
+
+
+typedef struct {
+    int   duration;
+    int   position;
+    int   paused;
+    float volume;
+    float speed;
+    int   shuffle;
+    int   loop;
+    int   playing;
+    char  current_file[256];
+} TomuStatus;
+
+
+
+static inline  void verr(const char *fmt, va_list ap)
+{
+	vfprintf(stderr, fmt, ap);
+	if (fmt[0] && fmt[strlen(fmt) - 1] == ':') {
+		fputc(' ', stderr);
+		perror(NULL);
+	} else {
+		fputc('\n', stderr);
+	}
+}
+
+static inline  void warn(const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	verr(fmt, ap);
+  va_end(ap);
+}
+
+static inline void die(const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+  verr(fmt, ap);
+	va_end(ap);
+	exit(-1);
+}
+
+#endif
