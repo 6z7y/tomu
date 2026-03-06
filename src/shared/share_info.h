efd4d32b60f36db676912251875e89eb356d230e
6z7y <eenanho24n@proton.me>
2026-03-06 03:09:26 +0300

    update

---
+++ src/shared/share_info.h
@@ -0,0 +1,19 @@
+#ifndef SHARE_H
+#define SHARE_H
+
+// server info
+#define SERVER_NAME "tomu"
+#define SERVER_VER "1.0.5_2"
+
+// client cli info
+#define CLIENT_CLI_NAME "tomucli"
+#define CLIENT_CLI_VER "0.0.1"
+
+// path socket
+#define SOCKET_PATH "/tmp/tomu-sock"
+#define MAX_CLIENT 6
+
+#define false 0
+#define true 1
+
+#endif
