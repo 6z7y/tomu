ec2556b94947cff21b8777c1bd7115fe105b2a43
6z7y <eenanho24n@proton.me>
2026-04-12 17:46:23 +0300

    improve client handling and add event checker

--- src/server/socket_utils.c
+++ src/server/socket_utils.c
@@ -10,6 +10,15 @@
 #include "control.h"
 #include "utils.h"
 
+static inline void client_die(int i, Client_connection *client)
+{
+  close(client[i].fd);
+  client[i].fd = -1;
+  client[i].active = 0;
+  client[i].pfd.fd = -1;
+  printf("client %d disconnected\n", i+1);
+}
+
 // add client active to poll struct
 int add_client_into_poll(struct pollfd *fds, Client_connection *client)
 {
@@ -17,7 +26,7 @@ int add_client_into_poll(struct pollfd *fds, Client_connection *client)
   for_each_num(MAX_CLIENT) {
     if (client[i].active) {
       fds[nfds] = client[i].pfd; // add client active to fds struct poll
-      nfds++; // continue to limit MAX_CLIENT
+      nfds++;
     }
   }
   return nfds; // number clients active
@@ -25,7 +34,7 @@ int add_client_into_poll(struct pollfd *fds, Client_connection *client)
 
 void broadcast_status(Client_connection *client)
 {
-  if (!ctx.playback_active || !ctx.state.ready) return;  // ← add this
+  if (!ctx.playback_active || !ctx.state.ready) return;
 
   TomuStatus status = {
     .duration  = ctx.state.duration,
@@ -39,23 +48,15 @@ void broadcast_status(Client_connection *client)
 
   for_each_num(MAX_CLIENT) {
     if (client[i].active) {
-      if (write(client[i].fd, &status, sizeof(status)) <= 0) {
+      write(client[i].fd, &status, sizeof(status));
         // client died while writing — clean up
-        printf("jhhhhhhhhhh here in broadcaststatus\n");
-        client_die(i, client);
-      }
+        // printf("jhhhhhhhhhh here in broadcaststatus\n");
+        // client_die(i, client);
+      // }
     }
   }
 }
 
-void client_die(int i, Client_connection *client)
-{
-  close(client[i].fd);
-  client[i].fd = -1;
-  client[i].active = 0;
-  client[i].pfd.fd = -1;
-  printf("client %d disconnected\n", i+1);
-}
 
 // when server has event see call it or make new client
 void accept_new_client(int *server_fd, int *client_fd, Client_connection *client)
@@ -84,14 +85,17 @@ void accept_new_client(int *server_fd, int *client_fd, Client_connection *client
   }
 }
 
+static Command cmd;
 
-void handle_client_events(int i, struct pollfd *fds, Client_connection *client)
+static void handle_client_events(int i, struct pollfd *fds, Client_connection *client)
 {
-  Command cmd;
+
   int n = read(client[i].fd, &cmd, sizeof(cmd));
 
+  // if client quit will read this
   if (n <= 0) {
     client_die(i, client);
+    printf("test here handle_client_event()\n");
     if (ctx.state.paused)
       playback_toggle(&ctx.state);
     return;  // ← return early, don't fall through
@@ -123,6 +127,20 @@ void handle_client_events(int i, struct pollfd *fds, Client_connection *client)
   broadcast_status(client);
 }
 
+void client_checker_event(int nfds, struct pollfd *fds, Client_connection *client)
+{
+  int index = 1; // begin from 1 for clients, server take 0
+  for_each_num(MAX_CLIENT) {
+    if (!client[i].active) continue;
+
+    if (index < nfds && fds[index].revents & POLLIN) {
+      handle_client_events(i, fds, client);
+    }
+    index++;
+
+  }
+}
+
 void *start_playback_thread(void *arg) {
     char *path = (char*)arg;
     playback_run(path, 0, 1);
@@ -149,4 +167,25 @@ void start_playback(char *path)
 
     pthread_create(&ctx.playback_thread, NULL, start_playback_thread, path);
     ctx.playback_active = 1;
+}
+
+// socket mode ( 1 = start ), ( 0 = close )
+void socket_mode(int mode, int *server_fd)
+{
+  unlink(SOCKET_PATH); // remove old socket file
+
+  if (mode) {
+    // initlize socket protocol
+    struct sockaddr_un addr = { .sun_family = AF_UNIX, .sun_path = SOCKET_PATH };
+    *server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (*server_fd < 0) die("Socket:");
+
+    // bind socket to file
+    if (bind(*server_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) die("Bind:");
+
+    // enter listen mode
+    if (listen(*server_fd, MAX_CLIENT) < 0) die("Listen:");
+  }
+
+  else close(*server_fd);
 }
--- src/server/socket_utils.h
+++ src/server/socket_utils.h
@@ -7,10 +7,10 @@
 int add_client_into_poll(struct pollfd *fds, Client_connection *client);
 void broadcast_status(Client_connection *client);
 void accept_new_client(int *server_fd, int *client_fd, Client_connection *client);
-void handle_client_events(int i, struct pollfd *fds, Client_connection *client);
-void client_die(int i, Client_connection *client);
+void client_checker_event(int nfds, struct pollfd *fds, Client_connection *client);
 void start_playback(char *path);
 void send_cmd(int sock, Command cmd);
 void *playback_thread_func(void *arg);
+void socket_mode(int mode, int *server_fd);
 
 #endif
