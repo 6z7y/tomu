21b8a5adb6e62f2c24295ce21e97f60f4476b279
6z7y <eenanho24n@proton.me>
2026-04-12 17:47:11 +0300

    simplify main loop and use client event checker

--- src/server/main.c
+++ src/server/main.c
@@ -2,58 +2,50 @@
 #include <poll.h>
 #include <signal.h>
 
-#include "control.h"
+#include "audio_data.h"
+#include "args.h"
 #include "socket_utils.h"
 #include "utils.h"
-#include "audio_data.h"
 
 int main(int argc, char **argv)
 {
   signal(SIGINT, sig_clean);
 
-      ctx.queue_count = 0;
+  ctx.queue_count = 0;
 
-  if (args_handle(argv[argc - 1]) == 1) goto bye;
+  if (args_handle(argv[argc - 1])) goto bye;
 
   // 1. init socket
   int *server_fd = &ctx.server_fd;
   int *client_fd = &ctx.client_fd;
   socket_mode(1, server_fd); // socket on
 
-  // 2. init client array
+  // 2. init poll
+  struct pollfd fds[1 + MAX_CLIENT]; // (0) = server | (1..MAX_CLIENT) = clients
+  fds[0] = (struct pollfd){ *server_fd, POLLIN };
+
+  // 3. init client array struct
   Client_connection client[MAX_CLIENT] = {0};
 
-  // 3. init poll
-  struct pollfd fds[1 + MAX_CLIENT];
-  fds[0] = (struct pollfd){.fd = *server_fd, .events = POLLIN };
-
-  // 4. main loop (server will stay here)
+  // main loop (server will stay here)
   while(1) {
-    // 5. add client active inside poll
+    // 4. add client active inside poll
     int nfds = add_client_into_poll(fds, client); // number clients active now
 
-    // 6. start poll mode
-    int ret = poll(fds, nfds, 100); // 100ms timeout
+    // 5. start poll mode
+    int ret = poll(fds, nfds, 100); // 100ms timeout for update status
 
-    // 7. broadcast status to all active clients on timeout
+    // send status struct to clients
     if (ret == 0) {
       if (ctx.playback_active) broadcast_status(client);
       continue;
     }
 
-    // 9. new connection enter
+    // Accept new Client
     if (fds[0].revents & POLLIN) accept_new_client(server_fd, client_fd, client);
 
-    // 10. read client keys
-    int index = 1; // begin from 1 for clients, server take 0
-    for_each_num(MAX_CLIENT) {
-      if (!client[i].active) continue;
-
-      if (index < nfds && fds[index].revents & POLLIN) {
-        handle_client_events(i, fds, client);
-      }
-      index++;
-    }
+    // check event control
+    client_checker_event(nfds, fds, client);
   }
 
   // 11. close socket
