f6275aebd1d2347578070ae8a5de08828fff7336
6z7y <eenanho24n@proton.me>
2026-04-25 14:02:08 +0300

    move error handling utilities to shared module

--- src/tomu/utils.c
+++ src/tomu/utils.c
@@ -1,8 +1,5 @@
 #include <libavformat/avformat.h>
 #include <libavcodec/codec.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <sys/socket.h>
@@ -10,45 +7,15 @@
 
 #include "socket_utils.h"
 #include "utils.h"
-#include "../shared/share_info.h"
-
 
 void sig_clean(int sig)
 {
   cleanUP();
-  socket_mode(0, &ctx.server_fd);
-  exit(0);
+  server_socket_mode(0, &ctx.server_fd); // socket off
+  _exit(0);
 }
 
 void cleanUP(){
   if (ctx.fmtCTX ) avformat_close_input(&ctx.fmtCTX);
   if (ctx.codecCTX ) avcodec_free_context(&ctx.codecCTX);
 }
-
-// void verr(const char *fmt, va_list ap)
-// {
-// 	vfprintf(stderr, fmt, ap);
-// 	if (fmt[0] && fmt[strlen(fmt) - 1] == ':') {
-// 		fputc(' ', stderr);
-// 		perror(NULL);
-// 	} else {
-// 		fputc('\n', stderr);
-// 	}
-// }
-//
-// void warn(const char *fmt, ...)
-// {
-// 	va_list ap;
-// 	va_start(ap, fmt);
-// 	verr(fmt, ap);
-//   va_end(ap);
-// }
-//
-// void die(const char *fmt, ...)
-// {
-// 	va_list ap;
-// 	va_start(ap, fmt);
-//   verr(fmt, ap);
-// 	va_end(ap);
-// 	exit(-1);
-// }
