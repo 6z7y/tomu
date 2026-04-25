6d78d20c0af9da190c2c82699666c2ae2e4b06ee
6z7y <eenanho24n@proton.me>
2026-04-25 14:27:49 +0300

    empty for now

--- src/tomugui/main.c
+++ src/tomugui/main.c
@@ -6,29 +6,30 @@
 #include <signal.h>
 
 #include "utils.h"
-#include "../shared/share_data.h"
+// #include "../shared/share_data.h"
 
 // init client ctx
-Client_CTX client_ctx = {0};
+// Client_CTX client_ctx = {0};
 
 int main(int argc, char **argv)
 {
-  signal(SIGINT, sig_clean); // when prog close by ctrl+c
-
-  // 1. connect to server
-  int *server_fd = &client_ctx.server_fd; // socket fd session between server
-  socket_mode(server_fd, 1);
-
-  // 2. send client type
-  ClientType my_type = CLIENT_TUI;
-  write(*server_fd, &my_type, sizeof(ClientType));
-
-  // 3. init poll
-  struct pollfd fds[2]; // handle 2 fd sync it
-  fds[0] = (struct pollfd){ .fd= *server_fd, .events= POLLIN };    // socket handle
-  // fds[1] = (struct pollfd) { .fd= STDIN_FILENO, .events= POLLIN }; // stdin termios handle
-
-  char key[8];
+  // run_commands("jj");
+  // signal(SIGINT, sig_clean); // when prog close by ctrl+c
+  //
+  // // 1. connect to server
+  // int *server_fd = &client_ctx.server_fd; // socket fd session between server
+  // socket_mode(server_fd, 1);
+  //
+  // // 2. send client type
+  // ClientType my_type = CLIENT_TUI;
+  // write(*server_fd, &my_type, sizeof(ClientType));
+  //
+  // // 3. init poll
+  // struct pollfd fds[2]; // handle 2 fd sync it
+  // fds[0] = (struct pollfd){ .fd= *server_fd, .events= POLLIN };    // socket handle
+  // // fds[1] = (struct pollfd) { .fd= STDIN_FILENO, .events= POLLIN }; // stdin termios handle
+  //
+  // char key[8];
 
   // 7. main loop
   // while(1) {
@@ -41,10 +42,10 @@ int main(int argc, char **argv)
   //
   //   if (fds[1].revents & POLLIN) {}
   // }
-  printf("not yet!");
+  // printf("not yet!");
 
 
-  socket_mode(&*server_fd, 0);
+  // socket_mode(&*server_fd, 0);
 
 bye:
   return 0;
--- src/tomutui/main.c
+++ src/tomutui/main.c
@@ -6,29 +6,29 @@
 #include <signal.h>
 
 #include "utils.h"
-#include "../shared/share_data.h"
+#include "../shared/SHARE_DATA.h"
 
 // init client ctx
-Client_CTX client_ctx = {0};
+// Client_CTX client_ctx = {0};
 
 int main(int argc, char **argv)
 {
-  signal(SIGINT, sig_clean); // when prog close by ctrl+c
+  // signal(SIGINT, sig_clean); // when prog close by ctrl+c
 
-  // 1. connect to server
-  int *server_fd = &client_ctx.server_fd; // socket fd session between server
-  socket_mode(server_fd, 1);
-
-  // 2. send client type
-  ClientType my_type = CLIENT_TUI;
-  write(*server_fd, &my_type, sizeof(ClientType));
-
-  // 3. init poll
-  struct pollfd fds[2]; // handle 2 fd sync it
-  fds[0] = (struct pollfd){ .fd= *server_fd, .events= POLLIN };    // socket handle
-  // fds[1] = (struct pollfd) { .fd= STDIN_FILENO, .events= POLLIN }; // stdin termios handle
-
-  char key[8];
+  // // 1. connect to server
+  // int *server_fd = &client_ctx.server_fd; // socket fd session between server
+  // socket_mode(server_fd, 1);
+  //
+  // // 2. send client type
+  // ClientType my_type = CLIENT_TUI;
+  // write(*server_fd, &my_type, sizeof(ClientType));
+  //
+  // // 3. init poll
+  // struct pollfd fds[2]; // handle 2 fd sync it
+  // fds[0] = (struct pollfd){ .fd= *server_fd, .events= POLLIN };    // socket handle
+  // // fds[1] = (struct pollfd) { .fd= STDIN_FILENO, .events= POLLIN }; // stdin termios handle
+  //
+  // char key[8];
 
   // 7. main loop
   // while(1) {
@@ -41,10 +41,10 @@ int main(int argc, char **argv)
   //
   //   if (fds[1].revents & POLLIN) {}
   // }
-  printf("not yet!");
+  // printf("not yet!");
 
 
-  socket_mode(&*server_fd, 0);
+  // socket_mode(&*server_fd, 0);
 
 bye:
   return 0;
