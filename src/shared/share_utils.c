1407775cf620aa40da638d2a5f38bb357af30872
6z7y <eenanho24n@proton.me>
2026-04-25 13:48:42 +0300

    add formatted output and command execution helpers

--- src/shared/share_utils.c
+++ src/shared/share_utils.c
@@ -3,6 +3,34 @@
 #include <string.h>
 #include <stdarg.h>
 
+#include "share_utils.h"
+
+char *format(const char *fmt, ...)
+{
+  // read args
+  static char buf[256];
+  va_list args;
+  va_start(args, fmt);
+    vsnprintf(buf, sizeof(buf), fmt, args);
+  va_end(args);
+
+  return buf;
+}
+
+// function run command
+void run_command(char *cmd)
+{
+  FILE *p;
+
+  if (!(p = popen(cmd, "r"))) { 
+    warn("cmd:");
+    return;
+  }
+
+  pclose(p);
+}
+
+/* error handle */
 void verr(const char *fmt, va_list ap)
 {
 	vfprintf(stderr, fmt, ap);
