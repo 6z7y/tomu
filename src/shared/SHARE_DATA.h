11e61e1c743903c9f04193b3846d3c828093ba9a
6z7y <eenanho24n@proton.me>
2026-04-25 14:27:05 +0300

    change name file header

---
+++ src/shared/SHARE_DATA.h
@@ -0,0 +1,32 @@
+#ifndef SHARE_DATA
+#define SHARE_DATA
+
+#define SOCKET_PATH "/tmp/tomu-sock" // path socket file
+#define MAX_CLIENT 6 // limit client connection
+
+// server info
+#define TOMU_NAME "tomu"
+#define TOMU_VER "1.7.0"
+
+// client info
+#define TOMUCLI_NAME "tomucli"
+#define TOMUCLI_VER "0.0.5"
+
+#define TOMUTUI_NAME "tomutui"
+#define TOMUTUI_VER "X.X.X"
+
+#define TOMUGUI_NAME "tomugui"
+#define TOMUGUI_VER "X.X.X"
+
+// Utils
+#define false 0
+#define true 1
+
+typedef enum { // types
+  CLIENT_CLI,
+  CLIENT_TUI,
+  CLIENT_GUI,
+
+} ClientType;
+
+#endif
