d7a5427c0f56f009393afd5294741467d319324d
6z7y <eenanho24n@proton.me>
2026-05-04 21:17:17 +0300

    update

--- src/shared/SHARE_DATA.h
+++ src/shared/SHARE_DATA.h
@@ -6,11 +6,11 @@
 
 // server info
 #define TOMU_NAME "tomu"
-#define TOMU_VER "1.1.1"
+#define TOMU_VER "1.1.2"
 
 // client info
 #define TOMUCLI_NAME "tomucli"
-#define TOMUCLI_VER "1.1.0"
+#define TOMUCLI_VER "1.1.1"
 
 #define TOMUTUI_NAME "tomutui"
 #define TOMUTUI_VER "X.X.X"
