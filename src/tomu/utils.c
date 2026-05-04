7377e9fd38282dfa2b3aadae7c5cb90aaca0a959
6z7y <eenanho24n@proton.me>
2026-05-04 20:19:34 +0300

    free queue and metadata memory on exit

--- src/tomu/utils.c
+++ src/tomu/utils.c
@@ -10,12 +10,21 @@
 
 void sig_clean(int sig)
 {
+  for (int i = 0; i < ctx.queue_count; i++)
+    free(ctx.queue_list[i]);
+
   cleanUP();
   server_socket_mode(0, &ctx.server_fd); // socket off
   _exit(0);
 }
 
 void cleanUP(){
-  if (ctx.fmtCTX ) avformat_close_input(&ctx.fmtCTX);
-  if (ctx.codecCTX ) avcodec_free_context(&ctx.codecCTX);
+  Audio_Metadata *m = &ctx.state.metadata;
+  free(m->title);   free(m->artist); free(m->album);
+  free(m->album_artist); free(m->genre);
+  free(m->date);    free(m->track);
+  memset(m, 0, sizeof(*m));  // reset pointers to NULL
+
+  if (ctx.fmtCTX)  avformat_close_input(&ctx.fmtCTX);
+  if (ctx.codecCTX) avcodec_free_context(&ctx.codecCTX);
 }
