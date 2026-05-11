9e9035d682d84e4e3b4e90992fb1d01e5175cbfa
6z7y <eenanho24n@proton.me>
2026-05-11 19:40:00 +0300

    cleanup presence on SIGINT & remove unnecessary code

--- src/tomu/utils.c
+++ src/tomu/utils.c
@@ -5,25 +5,22 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 
+#include "discord_integration.h"
 #include "socket_utils.h"
 #include "utils.h"
 
 void sig_clean(int sig)
 {
+  discord_cleanup();  // ADD THIS - cleanup on Ctrl+C
   for (int i = 0; i < ctx.queue_count; i++)
     free(ctx.queue_list[i]);
 
   cleanUP();
-  server_socket_mode(0, &ctx.server_fd); // socket off
+  server_socket_mode(&ctx.server_fd, 0); // socket off
   _exit(0);
 }
 
 void cleanUP(){
-  Audio_Metadata *m = &ctx.state.metadata;
-  free(m->title);   free(m->artist); free(m->album);
-  free(m->album_artist); free(m->genre);
-  free(m->date);    free(m->track);
-  memset(m, 0, sizeof(*m));  // reset pointers to NULL
 
   if (ctx.fmtCTX)  avformat_close_input(&ctx.fmtCTX);
   if (ctx.codecCTX) avcodec_free_context(&ctx.codecCTX);
--- src/tomu/utils.h
+++ src/tomu/utils.h
@@ -1,20 +1,11 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
-
 #include "control.h"
 #include "../shared/SHARE_DATA.h"
 #include "../shared/share_utils.h"
 
-
-
 void sig_clean(int sig);
 void cleanUP();
-
-// void verr(const char *fmt, va_list ap);
-// void warn(const char *fmt, ...);
-// void die(const char *fmt, ...);
 
 #endif
