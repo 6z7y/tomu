96c423d9316e08f6ce5191af6c2e1471c7cf6f78
6z7y <eenanho24n@proton.me>
2026-06-07 17:06:21 +0300

    add parse json

--- src/tomucli/backend.h
+++ src/tomucli/backend.h
@@ -4,6 +4,7 @@
 #include "CLIENT_DATA.h"
 #include "../shared/shared_control.h"
 
+void parse_json(const char *key);
 int update_status(int fd, int *was_playing, TomuStatus *status, PlaybackQueue *queue);
 void progress(TomuStatus *status, double current_time, int duration_time);
 
