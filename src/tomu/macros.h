5f57e2f4a2696b18330799266ef1ab15d5c2b08e
6z7y <eenanho24n@proton.me>
2026-07-01 04:04:32 +0300

    move macros fn to macors.h

--- src/tomu/macros.h
+++ src/tomu/macros.h
@@ -1,34 +1,18 @@
 #ifndef APP_MACROS_H
 #define APP_MACROS_H
 
+#include <unistd.h>
 
-// minimal functions
-#define WITH_LOCK(mutex) for (int _once = (pthread_mutex_lock(&(mutex)), 1); _once; _once = (pthread_mutex_unlock(&(mutex)), 0)) // pthread mutex
-
-#define read_now_normal_msg(fd) { char buf[128]; int n = read(fd, buf, sizeof(buf)-1); buf[n] = '\0'; printf("%s", buf); } // read normal msg socket
-
-/* for loop */
-#define LEN(a) ( sizeof(a) / sizeof(a[0]) ) // get size array
-#define for_each_arr(arr) for (int i=0; i<LEN(arr); i++) // for array
-#define for_each_num(n) for (int i=0; i<n; i++) // for normal for loop number set
-
-// server info
+// program info
 #define TOMU_NAME "tomu"
-#define TOMU_VER "1.3.4"
+#define TOMU_VER "1.3.5"
 
-// sleeping
-#define sleep_ms(n) usleep(1000*n)
-
-/* time durations */
-#define get_hour(a) ((int)a / 3600) // convert time to hour
-#define get_min(a) (((int)a % 3600) / 60) // convert time to min
-#define get_sec(a) ((int)a % 60) // convert time to sec
-
+// D-Bus settings
 #define BUS_NAME    "org.mpris.MediaPlayer2.tomu"
 #define OBJ_PATH    "/org/mpris/MediaPlayer2"
-#define IFACE_ROOT  "org.mpris.MediaPlayer2"
-#define IFACE_PLAYER "org.mpris.MediaPlayer2.Player"
-#define IFACE_PROPS "org.freedesktop.DBus.Properties"
+#define IFACE_ROOT  "org.mpris.MediaPlayer2" // tomu
+#define IFACE_PLAYER "org.mpris.MediaPlayer2.Player" // control
+#define IFACE_PROPS "org.freedesktop.DBus.Properties" // write/read info
 
 // Utils
 #define F_ALSE 0
@@ -40,5 +24,25 @@
 #define WHT "\e[0;37m"
 #define MAG "\e[0;35m"
 #define GRN "\e[0;32m"
+
+// checking from msg if url
+#define IS_URL(arg) (!strncmp(arg, "http://", 7)) || (!strncmp(arg, "https://", 8)) // 1=is_url, 0=not_url
+#define IS_PATH(arg) (access(arg, F_OK) == 0)
+
+// block of pthread_mutex lock/unlock
+#define WITH_LOCK(mutex) for (int _once = (pthread_mutex_lock(&(mutex)), 1); _once; _once = (pthread_mutex_unlock(&(mutex)), 0)) // pthread mutex
+
+/* for loop */
+#define LEN(a) ( sizeof(a) / sizeof(a[0]) ) // get size array
+#define for_each_arr(arr) for (size_t i=0; i<LEN(arr); i++) // for array
+#define for_each_num(n) for (size_t i=0; i<n; i++) // for normal loop number set
+
+// sleeping by micro sec
+#define sleep_ms(n) usleep(1000*n)
+
+/* time durations */
+#define get_hour(a) ((int)a / 3600) // convert time to hour
+#define get_min(a) (((int)a % 3600) / 60) // convert time to min
+#define get_sec(a) ((int)a % 60) // convert time to sec
 
 #endif
