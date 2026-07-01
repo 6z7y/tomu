8a0d6e0a2bec512f2315875a378006a84f99be64
6z7y <eenanho24n@proton.me>
2026-07-01 04:05:49 +0300

    remove debug IS_URL

--- src/tomu/main.c
+++ src/tomu/main.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <curl/curl.h>
 
+#include "macros.h"
 #include "structs.h"
 #include "args.h"
 #include "mpris.h"
@@ -12,30 +13,19 @@
 
 PlayBackContext tctx = {0};
 
-#define IS_URL(arg) (!strncmp(arg, "http://", 7)) || (!strncmp(arg, "https://", 8))
-
 int main(int argc, char **argv)
 {
   signal(SIGINT, sig_clean);
   signal(SIGPIPE, SIG_IGN);
 
-  printf("is url: %d\n", IS_URL(argv[argc-1]));
-  return 0;
-
-  first_init();
-
   if (argc > 1) {
       if (args_handle(argv[argc - 1])) {
-          if (!tctx.playback_active) {
-              goto bye;
-          }
+          if (!tctx.playback_active) exit(0);
       }
   }
 
+  first_init();
+
   // Main loop - handles D-Bus events and keeps the program running
   mpris_loop();
-
-bye:
-  curl_global_cleanup();
-  return 0;
 }
