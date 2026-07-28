f465502b9701f5e0bb3e82607df74fc5e21f332a
6z7y <eenanho24n@proton.me>
2026-07-28 14:26:54 +0300

    make ctx as local

--- src/tomu/args.c
+++ src/tomu/args.c
@@ -26,7 +26,7 @@ void help()
   );
 }
 
-void args_handle(int argc, char **argv)
+void args_handle(PlayBackContext *ctx, int argc, char **argv)
 {
   const char *loop_opts[]     = {"--loop", "loop", "-l", NULL};
   const char *shuffle_opts[]  = {"--shuffle", "shuffle", "-s", NULL};
@@ -34,22 +34,22 @@ void args_handle(int argc, char **argv)
   const char *help_opts[]     = {"--help", "help", "-h", NULL};
   const char *ver_opts[]      = {"--version", "version", "-v", NULL};
 
-  tctx.state.shuffle = 0;
-  tctx.state.loop = LOOP_NONE;
-  tctx.list.filter_files = 1;
+  ctx->state.shuffle = 0;
+  ctx->state.loop = LOOP_NONE;
+  ctx->list.filter_files = 1;
 
   for (int i = 1; i < argc; i++) {
     if (arg_check_opts(argc, argv, loop_opts)) {
       switch(arg_match_opts_with_values(&argc, argv, loop_opts, loop_vals)) {
-        case 0: tctx.state.loop = LOOP_NONE; break;
-        case 1: tctx.state.loop = LOOP_TRACK; break;
-        case 2: tctx.state.loop = LOOP_PLAYLIST; break;
+        case 0: ctx->state.loop = LOOP_NONE; break;
+        case 1: ctx->state.loop = LOOP_TRACK; break;
+        case 2: ctx->state.loop = LOOP_PLAYLIST; break;
         case -1: die("tomu: unknown loop mode '%s' try (none|track|playlist)", argv[i+1]); break;
-        default: tctx.state.loop = LOOP_NONE;
+        default: ctx->state.loop = LOOP_NONE;
       }
     }
-    if (arg_match_opts(&argc, argv, shuffle_opts) == 1) tctx.state.shuffle = 1;
-    if (arg_match_opt(&argc, argv, "--no-filter") == 1) tctx.list.filter_files = 0;
+    if (arg_match_opts(&argc, argv, shuffle_opts) == 1) ctx->state.shuffle = 1;
+    if (arg_match_opt(&argc, argv, "--no-filter") == 1) ctx->list.filter_files = 0;
     if (arg_match_opts(&argc, argv, help_opts) == 1) { help(); exit(0); }
     if (arg_match_opts(&argc, argv, ver_opts) == 1)  { printf("tomu: %s\n", TOMU_VER); exit(0); }
   }
@@ -57,6 +57,6 @@ void args_handle(int argc, char **argv)
   const char *src = argv[1];
 
   if (src) {
-    if (src_handle(src) < 0) die("invalid '%s':", src);
+    if (src_handle(ctx, src) < 0) die("invalid '%s':", src);
   }
 }
