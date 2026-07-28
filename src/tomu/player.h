c0f789d0500781493dfddae250e2ba28d99a456c
6z7y <eenanho24n@proton.me>
2026-07-28 14:32:40 +0300

    move playback controls, and handle playback, and disable url source

--- src/tomu/player.h
+++ src/tomu/player.h
@@ -4,17 +4,6 @@
 #include "../../libs/miniaudio.h"
 #include "structs.h"
 
-void playback_handle();
-int playback_run(const char *src);
-
-void playback_toggle(PlaybackStatus *state);
-void playback_stop(PlaybackStatus *state);
-void seek_playback(PlaybackStatus *state, dbus_int64_t offset);
-void playback_speed_defualt(PlaybackStatus *state);
-void playback_speed_increase(PlaybackStatus *state);
-void playback_speed_decrease(PlaybackStatus *state);
-void shuffle_toggle(PlaybackStatus *state);
-void playback_next_audio(PlaybackStatus *state);
-void playback_prev_audio(PlaybackStatus *state);
+int playback_run(PlayBackContext *ctx, const char *src);
 
 #endif
